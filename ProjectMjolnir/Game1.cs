a186b2afee019ab8c064d112c0fa8875bf145c39
Nicky S. Marcussen <Nickymarcussen@gmail.com>
2013-02-18 10:31:54 +0100

    Fix
    Fixes missing SLN

---
+++ ProjectMjolnir/Game1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace ProjectMjolnir
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+        KeyboardState KS;
+        Player Player1;
+        Texture2D Line;
+        Forge _Forge;
+
+        public void Echo(string Text)
+        {
+            System.Diagnostics.Debug.WriteLine(Text);
+        }
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            Echo("Test");
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+
+        protected override void LoadContent()
+        {
+
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            Player1 = new Player(_spriteBatch,new Vector2(100,100),Content);
+            _Forge = new Forge(Content,_spriteBatch);
+            Line = Content.Load<Texture2D>("Line");
+        }
+
+        protected override void UnloadContent()
+        {
+           
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+            if(GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed) {
+                this.Exit();
+            }
+
+            //Player1.update(GamePad.GetState(PlayerIndex.One);
+            Player1.update();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(Line,new Vector2(0,400),Color.White);
+
+            _Forge.Draw();
+            Player1.Draw();
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ProjectMjolnir/Player.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMjolnir
+{
+    class Player
+    {
+        SpriteBatch spriteBatch;
+        Vector2 Position;
+        Texture2D Skin;
+        ContentManager Content;
+        //Jumping
+        bool Jumping;
+        int JumpCache;
+        int JumpHeight;
+        int JumpHDefault = -10;
+
+        //General Options
+        int Weight = 5; //Speed of wich he falls
+        int Speed = 5;
+        
+        public Player(SpriteBatch SPB,Vector2 Start_Pos,ContentManager Cont)
+        {
+            spriteBatch = SPB;
+            Position = Start_Pos;
+            Content = Cont;
+
+            Skin = Content.Load<Texture2D>("Player1");
+        }
+
+        public void update()
+        {
+            //Basic Movement
+            if (GamePad.GetState(PlayerIndex.One).DPad.Left == ButtonState.Pressed) { Position.X -= Speed; }
+            if (GamePad.GetState(PlayerIndex.One).DPad.Right == ButtonState.Pressed) { Position.X += Speed; }
+            if (GamePad.GetState(PlayerIndex.One).Buttons.A == ButtonState.Pressed && !Jumping) { Jumping = true; JumpCache = (int)Position.Y; JumpHeight = JumpHDefault; }
+
+            //Gravity Emulator NOTE THE STATIC FAKE GROUND
+            if (Position.Y < 640 - 16 - Skin.Height && !Jumping) {
+                Position.Y += Weight;
+            } else if (Position.Y >= 640 + 16 && !Jumping) {
+                Position.Y = ((int)Position.Y / 16) * 16;
+            }
+
+            //Jumping
+            if (Jumping) {
+                System.Diagnostics.Debug.WriteLine(JumpCache);
+                Position.Y += JumpHeight;
+                JumpHeight++;
+                if (Position.Y >= JumpCache)
+                {
+                    Jumping = false;
+                    Position.Y = ((int)Position.Y / 16) * 16;
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            spriteBatch.Draw(Skin,Position,Color.White);
+        }
+
+    }
+    
+}
