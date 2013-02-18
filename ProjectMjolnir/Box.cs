ed328f77219889d3cb4a86b0198552b871f5ce06
Nicky S. Marcussen <Nickymarcussen@gmail.com>
2013-02-18 09:42:39 +0100

    First Upload

---
+++ ProjectMjolnir/Box.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMjolnir
+{
+    class Box
+    {
+        Vector2 Position;
+        SpriteBatch spriteBatch;
+        Texture2D Skin;
+
+        public Box(Vector2 position,SpriteBatch SPB,Texture2D skin)
+        {
+            Position = position;
+            spriteBatch = SPB;
+            Skin = skin;
+        }
+
+        public void AssignSkin(Texture2D NewSkin)
+        {
+            Skin = NewSkin;
+        }
+
+        public void Draw()
+        {
+            spriteBatch.Draw(Skin,Position,Color.White);
+        }
+
+    }
+}
